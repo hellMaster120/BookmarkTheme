425d2acfc011aab66b2f19cf916acee061fb0b55
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2022-08-24 10:29:36 -0400

    t

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Body = document.body;
 const Head = document.head;
 const JS = document.createElement("script")
-const Css = document.createElement("style")
+const Css = document.createElement("link")
+const CSSLink = ""
+const JSLink = ""
+
 JS.setAttribute("id","CustomJSClass")
 Css.setAttribute("id","CustomCssClass")
-
-
-
+Css.setAttribute("rel","stylesheet")
+Css.setAttribute("href",CSSLink)
+JS.setAttribute("src",JSLink)
 Body.appendChild(JS)
 Head.appendChild(Css)
 
